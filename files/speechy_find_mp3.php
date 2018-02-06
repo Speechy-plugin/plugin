76bb3d749abfd38cc297e80a5bb71673ec208c6c
Nicolas <info@kwante.com>
2018-02-06 13:23:20 +0100

    css changes for speechy icon

--- files/speechy_find_mp3.php
+++ files/speechy_find_mp3.php
@@ -4,13 +4,14 @@ function speechy_find_mp3($content){
 	global $post;
 	$post_id = $post->ID;
 	
-	    $custom_content = "";
+	$custom_content = "";
 		
 		/*// MP3 available?
 		$mp3_ready = get_post_meta( $post_id, 'mp3_ready', true );
 		
 		if($mp3_ready == '1'){*/
-			
+		
+		if(is_single()){
 			// Get mp3_url for this post
 			$sp_mp3 = get_post_meta( $post_id, 'mp3_url', true );
 		
@@ -18,7 +19,7 @@ function speechy_find_mp3($content){
 				$custom_content .= "<!-- speechy.io --><div class='speechy_mp3 clearfix'>";
 				$custom_content .= "<span class='player_title'>".PLAYER_TITLE."</span>";
 				$custom_content .= do_shortcode( '[audio src="'.$sp_mp3.'"]' );
-				$custom_content .= "<small class='powered_by'>Powered by <a href='https://www.speechy.io/?utm_source=plugin&utm_medium=link&utm_campaign=powered_by' target='_blank' title='Try Speechy' alt='text-to-speech wordpress plugin'><img src='". plugins_url() ."/speechy/images/Speechy_icon_logo_32px.png'</a></small>";
+				$custom_content .= "<small class='powered_by'>Powered by <a href='https://www.speechy.io/?utm_source=plugin&utm_medium=link&utm_campaign=powered_by' target='_blank' title='Try Speechy' alt='text-to-speech wordpress plugin'><img src='" . plugins_url( 'images/Speechy_icon_logo_32px.png', dirname(__FILE__) ) . "' ></a></small>";
 				$custom_content .= "<div class='download_link'><a href='".$sp_mp3."' onclick=\"ga('send', 'event', 'Button', 'Download MP3', 'Users blog', '0');\">Download</a></div>";
 				$custom_content .= "</div><!-- END speechy.io -->";
 				$custom_content .= $content;
@@ -27,10 +28,9 @@ function speechy_find_mp3($content){
 			}else{
 				return $content;
 			}
-		/*}else{
+		}else{
 			return $content;
-		}*/
-	
+		}
 }
 
 add_filter( 'the_content', 'speechy_find_mp3' );
